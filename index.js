1d56e515d1e5c88d21f49d55a530ad60873cf7c1
unknown <taha.iu.bd@gmail.com>
2022-05-22 10:04:27 +0600

    all-tool-get

--- index.js
+++ index.js
@@ -4,9 +4,33 @@ const port = process.env.PORT || 5000
 require('dotenv').config()
 const cors = require('cors')
 
+//Main Api
+//https://morning-ocean-16366.herokuapp.com/
 app.use(express.json())
 app.use(cors())
 
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tdvbv.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+})
+
+async function run() {
+    try {
+        await client.connect()
+        const toolsCollection = client.db('tools-maker').collection('tools')
+        app.get('/tools', async (req, res) => {
+            const result = await toolsCollection.find({}).toArray()
+            res.send(result)
+        })
+    } finally {
+        //
+    }
+}
+run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
