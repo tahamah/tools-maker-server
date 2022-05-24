b67b7b0a660947c916aad985fbec85f9adf5ff07
unknown <taha.iu.bd@gmail.com>
2022-05-24 14:37:24 +0600

    add-review-done

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-require('dotenv').config()
 const cors = require('cors')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+
+//middleware
+app.use(cors())
+app.use(express.json())
 
 //Main Api
 //https://morning-ocean-16366.herokuapp.com/
-app.use(express.json())
-app.use(cors())
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tdvbv.mongodb.net/?retryWrites=true&w=majority`
 
 const client = new MongoClient(uri, {
@@ -22,8 +25,37 @@ async function run() {
     try {
         await client.connect()
         const toolsCollection = client.db('tools-maker').collection('tools')
+        const profileCollection = client.db('tools-maker').collection('profile')
+        const reviewsCollection = client.db('tools-maker').collection('reviews')
+
+        //get all tools
+        //https://morning-ocean-16366.herokuapp.com/tools
         app.get('/tools', async (req, res) => {
-            const result = await toolsCollection.find({}).toArray()
+            const result = await toolsCollection.find({}).limit(4).toArray()
+            res.send(result)
+        })
+        //get all reviews
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find({}).toArray()
+            res.send(result)
+        })
+        //post reviews
+        app.post('/reviews', async (req, res) => {
+            const data = req.body
+            const result = await reviewsCollection.insertOne(data)
+            res.send(result)
+        })
+
+        app.post('/profile', async (req, res) => {
+            const data = req.body
+            const result = await profileCollection.insertOne(data)
+            res.send(result)
+        })
+
+        app.get('/profile/:email', async (req, res) => {
+            const email = req.params.email
+            const result = await toolsCollection.find({ email }).toArray()
+            console.log(result)
             res.send(result)
         })
     } finally {
@@ -36,5 +68,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`server running on port ${port}`)
 })
